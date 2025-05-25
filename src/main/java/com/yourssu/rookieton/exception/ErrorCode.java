8c0647ec5c7851d42c0551e090d552c04ff74d1b
chlee <chlee@student.42seoul.kr>
2025-05-25 18:53:27 +0900

    feat: error handler to custom error

---
+++ src/main/java/com/yourssu/rookieton/exception/ErrorCode.java
@@ -0,0 +1,32 @@
+package com.yourssu.rookieton.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+import static org.springframework.http.HttpStatus.*;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+    DUPLICATE_USER(CONFLICT, "이미 존재하는 유저입니다."),
+
+    USER_NOT_FOUND(NOT_FOUND, "유저가 존재하지 않습니다."),
+    ARTICLE_NOT_FOUND(NOT_FOUND, "게시글이 존재하지 않습니다."),
+    COMMENT_NOT_FOUND(NOT_FOUND, "댓글이 존재하지 않습니다."),
+
+    INVALID_PASSWORD(UNAUTHORIZED, "유저 패스워드가 일치하지 않습니다."),
+
+    MISMATCH_COMMENT_AUTHOR(FORBIDDEN, "댓글 작성자가 아닙니다."),
+    MISMATCH_ARTICLE_AUTHOR(FORBIDDEN, "게시글 작성자가 아닙니다."),
+
+    DUPLICATE_INTEREST_CATEGORY(CONFLICT, "중복된 카테고리가 들어왔습니다."),
+
+    EMPTY_INTEREST(BAD_REQUEST, "관심사가 비어있습니다."),
+    EMPTY_INTEREST_SUBCATEGORY(BAD_REQUEST, "관심사가 존재하지 않습니다."),
+    ;
+
+    private final HttpStatus httpStatus;
+    private final String message;
+
+}
